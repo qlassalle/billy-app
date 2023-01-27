04b771d076f567916b0fbdaea0cf9271f8ae38a9
Quentin Lassalle <quentin.lassalle@blablacar.com>
2023-01-27 22:44:15 +0100

    Introduce parameterized tests to increase readability

--- src/test/java/com/qlassalle/billy/CsvParserTest.java
+++ src/test/java/com/qlassalle/billy/CsvParserTest.java
@@ -1,43 +1,38 @@
 package com.qlassalle.billy;
 
 import com.qlassalle.billy.domain.Event;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.qlassalle.billy.Fixtures.buildExpectedObjectWithoutOptionalLocation;
 import static com.qlassalle.billy.Fixtures.buildExpectedObjectWithoutOptionalLocationAndLineUp;
+import static com.qlassalle.billy.Fixtures.buildFullEvent;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CsvParserTest {
 
     private final CsvParser parser = new CsvParser();
 
-    @Test
-    void parseCsvAndBuildObject() throws IOException {
-        var csv = new String(getClass().getClassLoader()
-                                       .getResourceAsStream("input/csv/event-with-all-fields.csv")
-                                       .readAllBytes());
-        var parsedLine = parser.parse(csv);
-        assertThat(parsedLine).isEqualTo(List.of(Fixtures.buildFullEvent()));
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("buildTestCases")
+    void parseCsvAndBuildObject(String testName, String path, Event event) throws IOException {
+        try (var inputStream = getClass().getClassLoader().getResourceAsStream(path)) {
+            var csv = new String(inputStream.readAllBytes());
+            var parsedLine = parser.parse(csv);
+            assertThat(parsedLine).isEqualTo(List.of(event));
+        }
     }
 
-    @Test
-    void parseCsvAndBuildObjectWithoutOptionalParameters() throws IOException {
-        var csv = new String(getClass().getClassLoader()
-                                       .getResourceAsStream("input/csv/event-without-optional-location.csv")
-                                       .readAllBytes());
-        var parsedLine = parser.parse(csv);
-        assertThat(parsedLine).isEqualTo(List.of(buildExpectedObjectWithoutOptionalLocation()));
-    }
-
-    @Test
-    void parseCsvAndBuildObjectWithoutOptionalLocationAndLineUp() throws IOException {
-        var csv = new String(getClass().getClassLoader()
-                                       .getResourceAsStream("input/csv/event-without-optional-location-and-line-up.csv")
-                                       .readAllBytes());
-        var parsedLine = parser.parse(csv);
-        assertThat(parsedLine).isEqualTo(List.of(buildExpectedObjectWithoutOptionalLocationAndLineUp()));
+    private static Stream<Arguments> buildTestCases() {
+        return Stream.of(
+                Arguments.of("Line with all fields", "input/csv/event-with-all-fields.csv", buildFullEvent()),
+                Arguments.of("No location", "input/csv/event-without-optional-location.csv", buildExpectedObjectWithoutOptionalLocation()),
+                Arguments.of("No line up", "input/csv/event-without-optional-location-and-line-up.csv", buildExpectedObjectWithoutOptionalLocationAndLineUp())
+        );
     }
 }
